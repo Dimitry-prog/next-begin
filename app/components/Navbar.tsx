8b60590931ad7ec5aa58bf4125dee72240cde20b
Dimitry-prog <kfifa3@gmail.com>
2023-03-21 22:23:30 +0300

    feat: Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 p-4 flex flex-col md:flex-row justify-between bg-slate-600 drop-shadow-xl">
+      <h1 className="grid place-content-center mb-2 md:mb-0 text-3xl font-bold text-white">
+        <Link href="/">Next Step</Link>
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
   return (
     <main className={inter.className}>
-
+      <div className="grid place-content-center mb-2 md:mb-0 text-3xl font-bold text-white">
+        Next Step
+      </div>
     </main>
   )
 }
