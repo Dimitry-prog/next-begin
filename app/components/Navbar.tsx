09a874aa5a48359b5a6c2d37b785d82ff006fc7e
Dimitry-prog <kfifa3@gmail.com>
2023-03-21 22:39:19 +0300

    feat: Search

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from "next/link";
+import Search from "@/app/components/Search";
 
 const Navbar = () => {
   return (
@@ -7,6 +8,7 @@ const Navbar = () => {
       <h1 className="grid place-content-center mb-2 md:mb-0 text-3xl font-bold text-white">
         <Link href="/">Next Step</Link>
       </h1>
+      <Search/>
     </nav>
   );
 };
---
+++ app/components/Search.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import React, {FormEvent, useState} from 'react';
+import {useRouter} from "next/navigation";
+
+const Search = () => {
+  const [search, setSearch] = useState<string>('');
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch('');
+    router.push(`/${search}/`);
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="w-50 flex justify-center md:justify-between">
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        type="text"
+        placeholder="Search..."
+        className="w-80 p-2 text-xl bg-white rounded-xl outline-none focus:bg-slate-200 transition-all duration-500"
+      />
+      <button
+        type="submit"
+        className="p-2 ml-2 text-xl font-bold bg-slate-300 rounded-xl active:scale-75 transition-all duration-500"
+      >
+        😹
+      </button>
+    </form>
+  );
+};
+
+export default Search;
